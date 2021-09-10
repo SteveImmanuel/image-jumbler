37df9d8199e0f0b08441a4cfb8437bfc589d451a
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-09-10 23:30:01 +0700

    Add arg parser

--- main.py
+++ main.py
@@ -1,77 +1,65 @@
 import random
-import sys
 import os
 import numpy as np
+import argparse
 from PIL import Image
 
-key = 'asdwqe'
+parser = argparse.ArgumentParser(description='Jumble all pixels in an image based on a key')
+required_args = parser.add_argument_group('required arguments')
+required_args.add_argument('-k', help='Encryption key', required=True)
+required_args.add_argument('-i', help='Input path', required=True)
+parser.add_argument('-m', help='Encrypt or decrypt, defaults to encrypt', choices=['encrypt', 'decrypt'], default='encrypt')
+parser.add_argument('-o', help='Output path (if ommited, result will be saved in curdir', required=False)
+
+args = parser.parse_args()
+key = args.k
 random.seed(key)
 
-filename = 'email_2.png'
-name, ext = os.path.splitext(filename)
-print('encrypting')
-original_img = Image.open(filename)
-# original_img.save('test.jpeg')
+relative_path = args.i
+basename, ext = os.path.splitext(relative_path)
+in_abs_path = os.path.join(os.path.abspath('.'), relative_path)
+out_abs_path = args.o
 
-# a = np.array(original_img)
-# # print(a)
+if args.m == 'encrypt':
+    print('encrypting...')
+    original_img = Image.open(in_abs_path)
 
-# # test = Image.open('test.jpg')
-# # b = np.array(test)
-# # print(a-b)
+    for i in range(original_img.size[0]):
+        for j in range(original_img.size[1]):
+            x = random.randrange(0, original_img.size[0])
+            y = random.randrange(0, original_img.size[1])
+            
+            pix1 = original_img.getpixel((i, j))
+            pix2 = original_img.getpixel((x, y))
+            original_img.putpixel((i, j), pix2)
+            original_img.putpixel((x, y), pix1)
+    
+    if not out_abs_path:
+        out_abs_path = os.path.join(os.path.abspath('.'), f'{basename}_enc{ext}')
+    original_img.save(out_abs_path)
 
-# test2 = Image.fromarray(a)
-# c = np.array(test2)
-# print(a-c)
-# test2.save('test2.jpeg')
+else:
+    enc_img = Image.open(in_abs_path)
+    generated_random_num = []
 
-# test2 = Image.open('test2.jpeg')
-# c = np.array(test2)
-# print(a-c)
+    print('recovering random number...')
+    for i in range(enc_img.size[0]):
+        for j in range(enc_img.size[1]):
+            x = random.randrange(0, enc_img.size[0])
+            y = random.randrange(0, enc_img.size[1])
+            generated_random_num.append((x,y))
 
-for i in range(original_img.size[0]):
-    for j in range(original_img.size[1]):
-        x = random.randrange(0, original_img.size[0])
-        y = random.randrange(0, original_img.size[1])
-        
-        pix1 = original_img.getpixel((i, j))
-        pix2 = original_img.getpixel((x, y))
-        original_img.putpixel((i, j), pix2)
-        original_img.putpixel((x, y), pix1)
+    print('decrypting...')
+    for i in range(enc_img.size[0] - 1, -1, -1):
+        for j in range(enc_img.size[1] - 1, -1, -1):
+            x, y = generated_random_num.pop()
+            pix1 = enc_img.getpixel((i, j))
+            pix2 = enc_img.getpixel((x, y))
+            enc_img.putpixel((i, j), pix2)
+            enc_img.putpixel((x, y), pix1)
 
-original_img.save(f'{name}_enc.{ext}')
-
-
-enc_img = Image.open(f'{name}_enc.{ext}')
-generated_random_num = []
-
-print('recovering random number')
-random.seed(key)
-
-for i in range(enc_img.size[0]):
-    for j in range(enc_img.size[1]):
-        x = random.randrange(0, enc_img.size[0])
-        y = random.randrange(0, enc_img.size[1])
-        generated_random_num.append((x,y))
-
-print('decrypting')
-
-for i in range(enc_img.size[0] - 1, -1, -1):
-    for j in range(enc_img.size[1] - 1, -1, -1):
-        x, y = generated_random_num.pop()
-        pix1 = enc_img.getpixel((i, j))
-        pix2 = enc_img.getpixel((x, y))
-        enc_img.putpixel((i, j), pix2)
-        enc_img.putpixel((x, y), pix1)
-
-enc_img.save(f'{name}_dec.{ext}')
-
-# original_img = Image.open('doge.jpg')
-# dec_img = Image.open('test.jpg')
-
-# for i in range(original_img.size[0]):
-#     for j in range(original_img.size[1]):
-#         pixel_original = original_img.getpixel((i, j))
-#         pixel_dec = dec_img.getpixel((i, j))
-#         print('dif', pixel_original[0] - pixel_dec[0], pixel_original[1] - pixel_dec[1], pixel_original[2] - pixel_dec[2])
-#     break
+    if not out_abs_path:
+        out_abs_path = os.path.join(os.path.abspath('.'), f'{basename}_dec{ext}')
+    enc_img.save(out_abs_path)
+    
+print('finished')
