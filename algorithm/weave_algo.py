b31d54924f60262c065359ecafdddac9420ab9f2
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-09-11 14:51:16 +0700

    Implement weave algorith

--- algorithm/weave_algo.py
+++ algorithm/weave_algo.py
@@ -1,12 +1,38 @@
+import random
 from .base_algo import BaseAlgo
 
 class WeaveAlgo(BaseAlgo):
     def recover_random_number(self):
-        return super().recover_random_number()
+        super().recover_random_number()
+
+        for i in range(self.img_arr.shape[0]):
+            rand_num = random.randrange(0, self.img_arr.shape[0])
+            self.generated_random_num.append(rand_num)
+        
+        for i in range(self.img_arr.shape[1]):
+            rand_num = random.randrange(0, self.img_arr.shape[1])
+            self.generated_random_num.append(rand_num)
     
     def encrypt(self):
-        return super().encrypt()
+        super().encrypt()
+
+        for i in range(self.img_arr.shape[0]):
+            rand_num = random.randrange(0, self.img_arr.shape[0])
+            self.img_arr[[i, rand_num]] = self.img_arr[[rand_num, i]]
+        
+        for i in range(self.img_arr.shape[1]):
+            rand_num = random.randrange(0, self.img_arr.shape[1])
+            self.img_arr[:, [i, rand_num]] = self.img_arr[:, [rand_num, i]]
+
 
     def decrypt(self):
-        return super().decrypt()
+        super().decrypt()
+        print(self.img_arr.shape)
+        for i in range(self.img_arr.shape[1] - 1, -1, -1):
+            rand_num = self.generated_random_num.pop()
+            self.img_arr[:, [i, rand_num]] = self.img_arr[:, [rand_num, i]]
+            
+        for i in range(self.img_arr.shape[0] - 1, -1, -1):
+            rand_num = self.generated_random_num.pop()
+            self.img_arr[[i, rand_num]] = self.img_arr[[rand_num, i]]
         
