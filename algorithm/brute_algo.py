a6ed19133862a16e663b4132e3b0ae38535a3c73
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-09-11 14:33:57 +0700

    Encapsulate brute logic

--- algorithm/brute_algo.py
+++ algorithm/brute_algo.py
@@ -1,12 +1,35 @@
+import random
 from .base_algo import BaseAlgo
 
 class BruteAlgo(BaseAlgo):
     def recover_random_number(self):
-        return super().recover_random_number()
+        super().recover_random_number()
+
+        for i in range(self.img_arr.shape[0]):
+            for j in range(self.img_arr.shape[1]):
+                x = random.randrange(0, self.img_arr.shape[0])
+                y = random.randrange(0, self.img_arr.shape[1])
+                self.generated_random_num.append((x,y))
     
     def encrypt(self):
-        return super().encrypt()
+        super().encrypt()
+
+        for i in range(self.img_arr.shape[0]):
+            for j in range(self.img_arr.shape[1]):
+                x = random.randrange(0, self.img_arr.shape[0])
+                y = random.randrange(0, self.img_arr.shape[1])
+                
+                temp = self.img_arr[i][j].copy() 
+                self.img_arr[i][j] = self.img_arr[x][y].copy() 
+                self.img_arr[x][y] = temp 
 
     def decrypt(self):
-        return super().decrypt()
-        
+        super().decrypt()
+
+        for i in range(self.img_arr.shape[0] - 1, -1, -1):
+            for j in range(self.img_arr.shape[1] - 1, -1, -1):
+                x, y = self.generated_random_num.pop()
+
+                temp = self.img_arr[i][j].copy() 
+                self.img_arr[i][j] = self.img_arr[x][y].copy() 
+                self.img_arr[x][y] = temp 
