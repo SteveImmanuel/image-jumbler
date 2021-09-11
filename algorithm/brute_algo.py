f6fa35f9199263403ab86a58fb58867c1c3147a8
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-09-11 14:06:59 +0700

    Fix syntax

--- algorithm/brute_algo.py
+++ algorithm/brute_algo.py
@@ -1,16 +1,12 @@
-from base_algo import BaseAlgo
-from abc import override
+from .base_algo import BaseAlgo
 
 class BruteAlgo(BaseAlgo):
-    @override
     def recover_random_number(self):
         return super().recover_random_number()
     
-    @override
     def encrypt(self):
         return super().encrypt()
 
-    @override
     def decrypt(self):
         return super().decrypt()
         
